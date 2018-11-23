8aaba15a88d7f804c07780625db3ae5cfafc78f4
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-23 05:50:55 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+let fromN = 0;
+let toN = 0;
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+  return toN + sum(fromN, toN -1);
+
 }
 
+console.log(sum(fromN, toN));
+
 module.exports = sum;
+
